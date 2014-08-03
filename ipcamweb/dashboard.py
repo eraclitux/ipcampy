ddb029b7b78ab18258e97c9061cac49cdb762c2a
eraclitux <eraclitux@gmail.com>
2014-08-03 18:25:16 +0200

    Add snaphshots browsing capabilities
    Close #4

--- ipcamweb/dashboard.py
+++ ipcamweb/dashboard.py
@@ -1,8 +1,9 @@
 # Provides a web interface for ipcampy
 # Andrea Masi 2014 eraclitux@gmail.com
 
+import datetime
 from flask import Flask, render_template, send_from_directory, redirect, session, request, url_for
-from utils import list_snapshots_days
+from utils import list_snapshots_days, list_snapshots_hours, list_snapshots_for_a_minute
 
 app = Flask("ipcamweb")
 USERNAME = "watcher"
@@ -23,15 +24,43 @@ def main():
     else:
         return redirect(url_for('login'))
 
-@app.route("/cam/<int:cam_id>")
-def cam_detail(cam_id):
-    days = list_snapshots_days(app.store_path, app.cams[cam_id - 1].cam_id)
-    return render_template("cam_detail.html", cam=app.cams[cam_id - 1])
+@app.route("/cam/<int:cam_index>")
+def cam_detail(cam_index):
+    days = list_snapshots_days(app.store_path, app.cams[cam_index - 1].cam_id)
+    return render_template("cam_detail.html", cam_index=cam_index, days=days, cam=app.cams[cam_index - 1])
 
-# FIXME stub
-@app.route("/get-image/<image>")
-def get_image(image):
-    return send_from_directory("/tmp", image)
+@app.route("/screenshots/<int:cam_index>/<day>")
+def screenshots_details(cam_index, day):
+    hours = list_snapshots_hours(app.store_path, app.cams[cam_index - 1].cam_id, day)
+    return render_template(
+            "screenshots_details.html", 
+	    day=(datetime.datetime.strptime(day, "%d%m%Y").strftime('%d/%m/%y'), day),
+            cam_index=cam_index, 
+            hours=hours, 
+            cam=app.cams[cam_index - 1],
+    )
+
+@app.route("/view-screenshots/<int:cam_index>/<day>/<hourm>")
+def view_screenshots(cam_index, day, hourm):
+    screenshots = list_snapshots_for_a_minute(
+            app.store_path,
+            app.cams[cam_index - 1].cam_id,
+            day,
+            hourm,
+    )
+    return render_template(
+            "view_screenshots.html",
+            cam_index=cam_index, 
+            day=day,
+            hourm=hourm,
+            screenshots=screenshots,
+            cam=app.cams[cam_index - 1],
+    )
+
+@app.route("/get-image/<int:cam_index>/<day>/<hour>/<image>")
+def get_image(cam_index, image, day, hour):
+    serve_path = app.store_path+"/"+app.cams[cam_index - 1].cam_id+"/"+day+"/"+hour
+    return send_from_directory(serve_path, image)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
--- ipcamweb/utils.py
+++ ipcamweb/utils.py
@@ -28,3 +28,24 @@ def list_snapshots_days(path, cam_id):
         return days
     else:
         return []
+
+def list_snapshots_hours(path, cam_id, day):
+    """Returns a list of hour/min in which snapshopts are present"""
+    screenshoots_path = path+"/"+str(cam_id)+"/"+day
+    if os.path.exists(screenshoots_path):
+        hours = []
+        for hour_dir in sorted(os.listdir(screenshoots_path)):
+            hrm = datetime.datetime.strptime(hour_dir, "%H%M").strftime('%H:%M')
+            hours.append((hrm, hour_dir))
+        return hours
+    else:
+        return []
+
+def list_snapshots_for_a_minute(path, cam_id, day, hourm):
+    """Returns a list of screenshots"""
+    screenshoots_path = path+"/"+str(cam_id)+"/"+day+"/"+hourm
+    if os.path.exists(screenshoots_path):
+        screenshots = [scr for scr in sorted(os.listdir(screenshoots_path))]
+        return screenshots
+    else:
+        return []
